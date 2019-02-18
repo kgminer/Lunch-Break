732e7ac2fcef701182cbf7abc32a696b9a3f370b
Zac G <gilliland.zachary@knights.ucf.edu>
2019-02-17 22:24:10 -0500

    Zac's Test Scene
    Zac's test facility is just a scene of me messing around with some things in unity. Simple movement controller and camera follow scripts are available

---
+++ Lunch Break/Assets/Zac'sTestFacility/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5f;
+
+    Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetCamPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPosition, smoothing * Time.deltaTime);
+    }
+}
---
+++ Lunch Break/Assets/Zac'sTestFacility/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigid;
+    int floorMask;
+    float camRayLen = 100f;
+
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigid = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        TurnWithMouse();
+        // Animate(h, v);
+    }
+
+    private void Move(float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigid.MovePosition(transform.position + movement);
+    }
+
+    private void TurnWithMouse()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLen, floorMask)) ;
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+            playerRigid.MoveRotation(newRotation);
+        }
+    }
+
+    private void Animate(float h, float v)
+    {
+        bool moving = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", moving);
+    }
+}
