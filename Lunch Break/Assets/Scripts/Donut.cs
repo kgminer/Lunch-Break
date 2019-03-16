d4f2a59b4cb19ec398cb18ff3ac748e875f2a01b
TimRigby <32720743+TimRigby@users.noreply.github.com>
2019-03-16 18:18:10 -0400

    Prefabs for Donut, Fries, and Drink spawn variant with attached item script

---
+++ Lunch Break/Assets/Scripts/Donut.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Donut : InventoryItemBase
+{
+    public override string Name
+    {
+        get
+        {
+            return "Donut";
+        }
+    }
+
+    public override void OnUse()
+    {
+        // TODO: Do something with the object
+        base.OnUse();
+    }
+}
