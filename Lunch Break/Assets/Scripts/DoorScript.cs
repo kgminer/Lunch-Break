8366382893ecc818cfa286f26b377426ebeb0987
kgminer <k.gerhart@comcast.net>
2019-03-25 01:54:24 -0400

    Door Opening/Closing kind of working

---
+++ Lunch Break/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetTrigger("OpenDoor");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.enabled = true;
+    }
+
+    private void PauseAnimationEvent()
+    {
+        anim.enabled = false;
+    }
+}
