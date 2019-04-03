4fb7d36b4b06fedb6b72cbc50d71c27e07dcbdf3
kgminer <k.gerhart@comcast.net>
2019-04-03 02:29:01 -0400

    Adding Sounds on UI elements

--- Lunch Break/Assets/Scripts/Management/SoundManager.cs
+++ Lunch Break/Assets/Scripts/Management/SoundManager.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public AudioClip hitSound;
+    public AudioClip uiSelection;
 
     public void HitSound(Transform t)
     {
         AudioSource.PlayClipAtPoint(hitSound, t.position);
+    }
+
+    public void UISound(Transform t)
+    {
+        AudioSource.PlayClipAtPoint(uiSelection, t.position);
     }
 }
